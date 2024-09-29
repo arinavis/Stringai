a2bccd92393fc240eb704b6ae7de5f0e988cba83
arinavis <avisnevskaja@gmail.com>
2024-09-29 20:08:23 +0300

    Namų darbai

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
+# 1 uzd
+aktoriai = ['Johny Depp', 'Jensen Ackles']
+trump_str = min(aktoriai, key=len)
+print('Trumpesnis vardas yra: ', trump_str)
+
+name = "Johny"
+surname = "depp"
+
+if len(name) > len(surname):
+    print(surname)
+else:
+    print(name)
+
+# 2 uzd.
+
+vardas = 'Leonardo'
+pavarde = 'Dicaprio'
+
+print(vardas.upper(), pavarde.lower())
+
+# 3 uzd
+name1 = 'Angelina'
+surname1 = 'Jolie'
+first_latters = name1[0] + surname1[0]
+print(first_latters)
+
+# 4 uzd
+actorName = 'Brad'
+actorSurname = 'Pitt'
+short_name = actorName[-3:] + actorSurname[-3:]
+print(short_name)
+
+# kaip veikia?
+
+
+
 
