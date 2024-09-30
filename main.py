a528faea24c1e9042ecad44d700f843131e9216b
arinavis <avisnevskaja@gmail.com>
2024-09-30 20:51:16 +0300

    2024-09-30

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import re
+import random
+import string
 # 1 uzd
 aktoriai = ['Johny Depp', 'Jensen Ackles']
 trump_str = min(aktoriai, key=len)
@@ -22,8 +24,8 @@ print(vardas.upper(), pavarde.lower())
 # 3 uzd
 name1 = 'Angelina'
 surname1 = 'Jolie'
-first_latters = name1[0] + surname1[0]
-print(first_latters)
+first_letters = name1[0] + surname1[0]
+print(first_letters)
 
 # 4 uzd
 actorName = 'Brad'
@@ -54,3 +56,28 @@ print(new_name2)
 old_name3 = "It's a Wonderful Life"
 new_name3 = re.sub('[AaĄąEeĖėIiĮįYyOoUuŲųŪū]', '', old_name3)
 print(new_name3)
+
+# 7 uzd
+starWars = "Star Wars: Episode " + (" " * random.randint(1, 9)) + str(random.randint(1, 7)) + " - A New Hope"
+print(starWars)
+
+result = re.search(r'\d', starWars)
+print(result.group())
+
+print(starWars[-14:-13])
+print(re.sub(r"\D+","", starWars))
+
+# 8 uzd.
+
+fraze = "Don't Be a Menace to South Central While Drinking Your Juice in the Hoo"
+
+
+# 9 uzd.
+raides1 = random.choice(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+raides2 = random.choice(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+raides3 = random.choice(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+
+print(raides1 + raides2 + raides3)
+
+random_string = ''.join(random.sample(string.ascii_lowercase, 3))
+print(random_string)
